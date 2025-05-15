d1c4e5bc3580840f7494339d9bf68a869e8e180f
IT24101010 <it24101010@my.sliit.lk>
2025-05-16 00:33:42 +0530

    Add Movie class

---
+++ src/main/java/com/PGN15/movie_booking_app/model/Movie.java
@@ -0,0 +1,46 @@
+package com.PGN15.movie_booking_app.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie {
+    private String movieId;
+    private String title;
+    private String genre;
+    private String director;
+    private List<String> cast;
+    private int duration;
+    private String rating;
+    private String synopsis;
+    private double price;
+    private String posterUrl;
+    private String bannerUrl;
+
+
+    public Movie(String title, String genre, String director, List<String> cast, int duration, String rating, String synopsis, double price, String posterUrl, String bannerUrl) {
+        this.movieId = UUID.randomUUID().toString();
+        this.title = title;
+        this.genre = genre;
+        this.director = director;
+        this.cast = (cast == null) ? new ArrayList<>() : new ArrayList<>(cast); // Handle null cast
+        this.duration = duration;
+        this.rating = rating;
+        this.synopsis = synopsis;
+        this.price = price;
+        this.posterUrl = posterUrl;
+        this.bannerUrl = bannerUrl;
+    }
+
+
+
+}
